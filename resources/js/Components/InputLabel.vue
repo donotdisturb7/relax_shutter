fd896eb84c6ba8b9cf08db6ed2b39d979933d22e
donotdisturb7 <renalddesire55@gmail.com>
2024-10-04 13:01:47 -0400

    login

--- resources/js/Components/InputLabel.vue
+++ resources/js/Components/InputLabel.vue
@@ -7,7 +7,7 @@ defineProps({
 </script>
 
 <template>
-    <label class="block text-sm font-medium text-gray-700 dark:text-gray-300">
+    <label class="lock text-sm font-medium leading-6 text-gray-900">
         <span v-if="value">{{ value }}</span>
         <span v-else><slot /></span>
     </label>
--- resources/js/Components/TextInput.vue
+++ resources/js/Components/TextInput.vue
@@ -19,7 +19,7 @@ defineExpose({ focus: () => input.value.focus() });
 
 <template>
     <input
-        class="rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 dark:focus:border-indigo-600 dark:focus:ring-indigo-600"
+        class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
         v-model="model"
         ref="input"
     />
