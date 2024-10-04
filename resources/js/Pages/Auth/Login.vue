c14b95e171c3c064dd71a24096559aaa4d02400a
donotdisturb7 <renalddesire55@gmail.com>
2024-10-03 21:17:39 -0400

    add

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -94,6 +94,15 @@ const submit = () => {
                 >
                     Log in
                 </PrimaryButton>
+                <div>
+                    <Link
+                  
+                    :href="route('register')"
+                    class="rounded-md text-sm text-gray-600 underline hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:text-gray-400 dark:hover:text-gray-100 dark:focus:ring-offset-gray-800"
+                >
+                   Not registerd yet?
+                </Link>
+                    </div>
             </div>
         </form>
     </GuestLayout>
--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -20,7 +20,7 @@ const submit = () => {
 };
 </script>
 
-<template>
+<!-- <template>
     <GuestLayout>
         <Head title="Register" />
 
@@ -110,4 +110,27 @@ const submit = () => {
             </div>
         </form>
     </GuestLayout>
-</template>
+</template> -->
+
+<template>
+    <div class=" flex flex-col-reverse py-16 lg:pt-0 lg:flex-col lg:pb-0">
+      <div class="inset-y-0 top-0 right-0 z-0 w-full max-w-xl px-4 mx-auto md:px-0 lg:pr-0 lg:mb-0 lg:mx-0 lg:w-7/12 lg:max-w-full lg:absolute xl:px-0">
+        <svg class="absolute left-0 hidden h-full text-white transform -translate-x-1/2 lg:block" viewBox="0 0 100 100" fill="currentColor" preserveAspectRatio="none slice">
+          <path d="M50 0H100L50 100H0L50 0Z"></path>
+        </svg>
+        <div class=" bg-slate-950 object-cover w-full h-56 rounded shadow-lg lg:rounded-none lg:shadow-none md:h-96 lg:h-full
+        "></div>
+        
+      </div>
+      <div class="relative flex flex-col items-start w-full max-w-xl px-4 mx-auto md:px-0 lg:px-8 lg:max-w-screen-xl">
+        <div class="mb-16 lg:my-40 lg:max-w-lg lg:pr-5">
+  
+
+          <div class="flex items-center">
+
+           
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
