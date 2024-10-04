b1fe8806863fa5e4ca551811145f2d09b62a4bf0
Harrow973 <116327514+Harrow973@users.noreply.github.com>
2024-10-04 18:42:58 -0400

    gitignore2

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -7,26 +7,25 @@ import PrimaryButton from "@/Components/PrimaryButton.vue";
 import TextInput from "@/Components/TextInput.vue";
 import { Head, Link, useForm } from "@inertiajs/vue3";
 
-
 defineProps({
-  canResetPassword: {
-    type: Boolean,
-  },
-  status: {
-    type: String,
-  },
+    canResetPassword: {
+        type: Boolean,
+    },
+    status: {
+        type: String,
+    },
 });
 
 const form = useForm({
-  email: "",
-  password: "",
-  remember: false,
+    email: "",
+    password: "",
+    remember: false,
 });
 
 const submit = () => {
-  form.post(route("login"), {
-    onFinish: () => form.reset("password"),
-  });
+    form.post(route("login"), {
+        onFinish: () => form.reset("password"),
+    });
 };
 </script>
 
@@ -110,111 +109,123 @@ const submit = () => {
 </template> -->
 
 <template>
-  <div class="flex flex-col-reverse py-16 lg:pt-0 lg:flex-col lg:pb-0">
-    <div
-      class="inset-y-0 top-0 right-0 z-0 w-full max-w-xl px-4 mx-auto md:px-0 lg:pr-0 lg:mb-0 lg:mx-0 lg:w-7/12 lg:max-w-full lg:absolute xl:px-0"
-    >
-      <svg
-        class="absolute left-0 hidden h-full text-white transform -translate-x-1/2 lg:block"
-        viewBox="0 0 100 100"
-        fill="currentColor"
-        preserveAspectRatio="none slice"
-      >
-        <path d="M50 0H100L50 100H0L50 0Z"></path>
-      </svg>
-
-      <img
-        class="object-cover w-full h-56 rounded shadow-lg lg:rounded-none lg:shadow-none md:h-96 lg:h-full"
-        src="https://images.pexels.com/photos/3184291/pexels-photo-3184291.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
-        alt=""
-      />
-      
-    </div>
-    <div
-      class="relative flex flex-col items-start w-full max-w-xl px-4 mx-auto md:px-0 lg:px-8 lg:max-w-screen-xl"
-    >
-      <div class="mb-16 lg:my-40 lg:max-w-lg lg:pr-5">
+    <Head title="Connexion" />
+    <div class="flex flex-col-reverse py-16 lg:pt-0 lg:flex-col lg:pb-0">
         <div
-          class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8"
+            class="inset-y-0 top-0 right-0 z-0 w-full max-w-xl px-4 mx-auto md:px-0 lg:pr-0 lg:mb-0 lg:mx-0 lg:w-7/12 lg:max-w-full lg:absolute xl:px-0"
         >
-          <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-            <img class="mx-auto h-10 w-auto" src="/assets/logoRF.png" />
-            <h2
-              class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900"
+            <svg
+                class="absolute left-0 hidden h-full text-white transform -translate-x-1/2 lg:block"
+                viewBox="0 0 100 100"
+                fill="currentColor"
+                preserveAspectRatio="none slice"
             >
-              Sign in to your account
-            </h2>
-          </div>
+                <path d="M50 0H100L50 100H0L50 0Z"></path>
+            </svg>
 
-          <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-            <form class="space-y-6" @submit.prevent="submit">
-              <div>
-                <div class="mt-2">
-                  <InputLabel for="email" value="Email" />
-
-                  <TextInput
-                    id="email"
-                    type="email"
-                    class="mt-1 block w-full"
-                    v-model="form.email"
-                    required
-                    autofocus
-                    autocomplete="email"
-                  />
-
-                  <InputError class="mt-2" :message="form.errors.email" />
-                </div>
-              </div>
-
-              <div>
-                <div class="flex items-center justify-between"></div>
-                <div class="mt-2">
-                  <InputLabel for="password" value="Password" />
-                  <TextInput
-                    id="password"
-                    type="password"
-                    class="mt-1 block w-full"
-                    v-model="form.password"
-                    required
-                    autocomplete="current-password"
-                  />
-                  <InputError class="mt-2" :message="form.errors.password" />
-                </div>
-                <Link
-                  v-if="canResetPassword"
-                  :href="route('password.request')"
-                  class="font-semibold text-indigo-600 hover:text-indigo-500"
-                >
-                  Forgot your password?
-                </Link>
-              </div>
-
-              <div>
-                <PrimaryButton
-                  class="ms-4"
-                  :class="{ 'opacity-25': form.processing }"
-                  :disabled="form.processing"
-                >
-                  Log in
-                </PrimaryButton>
-              </div>
-            </form>
-
-            <Link
-              :href="route('register')"
-              class="font-semibold leading-6 text-indigo-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:text-gray-400 dark:focus:ring-offset-gray-800"
-            >
-              Not a member ?
-              <a
-                class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
-                >Register now</a
-              >
-            </Link>
-          </div>
+            <img
+                class="object-cover w-full h-56 rounded shadow-lg lg:rounded-none lg:shadow-none md:h-96 lg:h-full"
+                src="assets/FontLogin.avif"
+                alt=""
+            />
         </div>
-      </div>
-    </div>
-  </div>
-</template>
-  
+        <div
+            class="relative flex flex-col items-start w-full max-w-xl px-4 mx-auto md:px-0 lg:px-8 lg:max-w-screen-xl"
+        >
+            <div class="mb-16 lg:my-40 lg:max-w-lg lg:pr-5">
+                <div
+                    class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8"
+                >
+                    <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+                        <img
+                            class="mx-auto h-10 w-auto"
+                            src="/assets/logoRF.png"
+                        />
+                        <h2
+                            class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900"
+                        >
+                            Accédez à votre compte
+                        </h2>
+                    </div>
 
+                    <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                        <form class="space-y-6" @submit.prevent="submit">
+                            <div>
+                                <div class="mt-2">
+                                    <InputLabel for="email" value="Email" />
+
+                                    <TextInput
+                                        id="email"
+                                        type="email"
+                                        class="mt-1 block w-full"
+                                        v-model="form.email"
+                                        required
+                                        autofocus
+                                        autocomplete="email"
+                                    />
+
+                                    <InputError
+                                        class="mt-2"
+                                        :message="form.errors.email"
+                                    />
+                                </div>
+                            </div>
+
+                            <div>
+                                <div
+                                    class="flex items-center justify-between"
+                                ></div>
+                                <div class="mt-2">
+                                    <InputLabel
+                                        for="password"
+                                        value="Password"
+                                    />
+                                    <TextInput
+                                        id="password"
+                                        type="password"
+                                        class="mt-1 block w-full"
+                                        v-model="form.password"
+                                        required
+                                        autocomplete="current-password"
+                                    />
+                                    <InputError
+                                        class="mt-2"
+                                        :message="form.errors.password"
+                                    />
+                                </div>
+                                <Link
+                                    v-if="canResetPassword"
+                                    :href="route('password.request')"
+                                    class="font-semibold text-indigo-600 hover:text-indigo-500"
+                                >
+                                    Mot de passe oublié ?
+                                </Link>
+                            </div>
+
+                            <div>
+                                <PrimaryButton
+                                    class="ms-4"
+                                    :class="{ 'opacity-25': form.processing }"
+                                    :disabled="form.processing"
+                                >
+                                    Se connecter
+                                </PrimaryButton>
+                            </div>
+                        </form>
+
+                        <Link
+                            :href="route('register')"
+                            class="font-semibold leading-6 text-indigo-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:text-gray-400 dark:focus:ring-offset-gray-800"
+                        >
+                            Vous n’avez pas de compte
+                            <a
+                                class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+                                >S'inscrire</a
+                            >
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
