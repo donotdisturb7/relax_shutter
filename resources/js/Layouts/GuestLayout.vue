9815a3f50376a0742ac01b6ace296c41cf350e07
donotdisturb7 <renalddesire55@gmail.com>
2024-10-04 22:22:48 -0400

    cleaning

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -6,13 +6,11 @@ import { Link } from '@inertiajs/vue3';
 <template>
     <div>
         <div>
-            <Link href="/">
-                <ApplicationLogo class="h-20 w-20 fill-current text-gray-500" />
-            </Link>
+        
         </div>
 
         <div
-            class="mt-6 w-full overflow-hidden bg-white px-6 py-4 shadow-md sm:max-w-md sm:rounded-lg dark:bg-gray-800"
+            class=""
         >
             <slot />
         </div>
