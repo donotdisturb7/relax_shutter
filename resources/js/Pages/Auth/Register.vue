7d7b2fba2b3366a31de30b8e675f455290996be1
Harrow973 <116327514+Harrow973@users.noreply.github.com>
2024-10-08 16:18:35 -0400

    home

--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -42,12 +42,19 @@ export default {
     <Head title="Inscription" />
     <div class="relative flex flex-col lg:flex-row min-h-screen">
         <!-- Partie formulaire d'inscription -->
-        <div class="relative flex flex-col items-center justify-center w-full max-w-xl px-4 mx-auto md:px-0 lg:max-w-screen-xl lg:justify-start lg:w-5/12 z-10 bg-white rounded-lg p-8">
+        <div
+            class="relative flex flex-col items-center justify-center w-full max-w-xl px-4 mx-auto md:px-0 lg:max-w-screen-xl lg:justify-start lg:w-5/12 z-10 bg-white rounded-lg p-8"
+        >
             <div class="mb-16 lg:my-40 lg:max-w-lg lg:pr-5">
                 <div class="flex min-h-full flex-1 flex-col justify-center">
                     <div class="sm:mx-auto sm:w-full sm:max-w-sm text-center">
-                        <img class="mx-auto h-10 w-auto" src="/assets/logoRF.png" />
-                        <h2 class="mt-10 text-2xl font-bold leading-9 tracking-tight text-gray-900">
+                        <img
+                            class="mx-auto h-10 w-auto"
+                            src="/assets/logoRF.png"
+                        />
+                        <h2
+                            class="mt-10 text-2xl font-bold leading-9 tracking-tight text-gray-900"
+                        >
                             Créez votre compte.
                         </h2>
                     </div>
@@ -65,7 +72,10 @@ export default {
                                     autofocus
                                     autocomplete="name"
                                 />
-                                <InputError class="mt-2" :message="form.errors.name" />
+                                <InputError
+                                    class="mt-2"
+                                    :message="form.errors.name"
+                                />
                             </div>
 
                             <div>
@@ -78,14 +88,22 @@ export default {
                                     required
                                     autocomplete="username"
                                 />
-                                <InputError class="mt-2" :message="form.errors.email" />
+                                <InputError
+                                    class="mt-2"
+                                    :message="form.errors.email"
+                                />
                             </div>
 
                             <div>
-                                <InputLabel for="password" value="Mot de passe" />
+                                <InputLabel
+                                    for="password"
+                                    value="Mot de passe"
+                                />
                                 <div class="relative">
                                     <input
-                                        :type="showPassword ? 'text' : 'password'"
+                                        :type="
+                                            showPassword ? 'text' : 'password'
+                                        "
                                         v-model="form.password"
                                         id="password"
                                         class="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
@@ -108,9 +126,11 @@ export default {
                                             <path
                                                 stroke-linecap="round"
                                                 stroke-linejoin="round"
-                                                :d="showPassword
-                                                    ? 'M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88'
-                                                    : 'M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z'"
+                                                :d="
+                                                    showPassword
+                                                        ? 'M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88'
+                                                        : 'M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z'
+                                                "
                                             />
                                             <path
                                                 v-if="!showPassword"
@@ -121,14 +141,24 @@ export default {
                                         </svg>
                                     </button>
                                 </div>
-                                <InputError class="mt-2" :message="form.errors.password" />
+                                <InputError
+                                    class="mt-2"
+                                    :message="form.errors.password"
+                                />
                             </div>
 
                             <div>
-                                <InputLabel for="password_confirmation" value="Confirmez le mot de passe" />
+                                <InputLabel
+                                    for="password_confirmation"
+                                    value="Confirmez le mot de passe"
+                                />
                                 <div class="relative">
                                     <input
-                                        :type="showConfirmPassword ? 'text' : 'password'"
+                                        :type="
+                                            showConfirmPassword
+                                                ? 'text'
+                                                : 'password'
+                                        "
                                         v-model="form.password_confirmation"
                                         id="password_confirmation"
                                         class="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
@@ -151,9 +181,11 @@ export default {
                                             <path
                                                 stroke-linecap="round"
                                                 stroke-linejoin="round"
-                                                :d="showConfirmPassword
-                                                    ? 'M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88'
-                                                    : 'M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z'"
+                                                :d="
+                                                    showConfirmPassword
+                                                        ? 'M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88'
+                                                        : 'M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z'
+                                                "
                                             />
                                             <path
                                                 v-if="!showConfirmPassword"
@@ -164,7 +196,10 @@ export default {
                                         </svg>
                                     </button>
                                 </div>
-                                <InputError class="mt-2" :message="form.errors.password_confirmation" />
+                                <InputError
+                                    class="mt-2"
+                                    :message="form.errors.password_confirmation"
+                                />
                             </div>
 
                             <div>
@@ -182,7 +217,10 @@ export default {
                             class="font-semibold leading-6 text-indigo-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:text-gray-400 dark:focus:ring-offset-gray-800"
                         >
                             Vous avez déjà un compte ?
-                            <span class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Se connecter</span>
+                            <span
+                                class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+                                >Se connecter</span
+                            >
                         </Link>
                     </div>
                 </div>
@@ -197,7 +235,7 @@ export default {
                 fill="currentColor"
                 preserveAspectRatio="none slice"
             >
-                <path d="M50 0H100L50 100H0L50 0Z"></path>
+                <!-- <path d="M50 0H100L50 100H0L50 0Z"></path> -->
             </svg>
             <img
                 class="object-cover w-full h-screen rounded-lg lg:rounded-none lg:shadow-none"
@@ -207,3 +245,5 @@ export default {
         </div>
     </div>
 </template>
+
+
--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,31 +1,69 @@
 <script setup>
-import {Head, Link} from '@inertiajs/vue3';
-
+import { ref } from "vue";
+import { Head, Link } from "@inertiajs/vue3";
 import CreatePost from "@/Components/app/CreatePost.vue";
 import PostList from "@/Components/app/PostList.vue";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 
-defineProps({
+const props = defineProps({
     posts: Object,
-    followings: Array
+    followings: Array,
 });
+
+const showAlert = ref(false);
+
+const handlePostCreated = () => {
+    showAlert.value = true;
+    setTimeout(() => {
+        showAlert.value = false;
+    }, 3000);
+};
 </script>
 
 <template>
-    <Head title="Relax Shutter"/>
-
+    <Head title="Relax Shutter" />
     <AuthenticatedLayout>
+        <div class="container mx-auto px-4 py-8">
+            <h1 class="text-3xl font-bold mb-8 text-center">
+                Bienvenue sur Relax Shutter
+            </h1>
 
-        <div class="">
- 
-            <div class="">
-                <CreatePost/>
-                <PostList :posts="posts.data" class="flex-1"/>
+            <div class="grid grid-cols-1 md:grid-cols-3 gap-8">
+                <div class="md:col-span-2">
+                    <CreatePost @post-created="handlePostCreated" />
+                    <div
+                        v-if="showAlert"
+                        class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative mt-4"
+                        role="alert"
+                    >
+                        <strong class="font-bold">Succès!</strong>
+                        <span class="block sm:inline">
+                            Votre post a été créé avec succès.</span
+                        >
+                    </div>
+                    <PostList :posts="posts.data" class="mt-8" />
+                </div>
+
+                <div class="bg-white shadow rounded-lg p-6">
+                    <h2 class="text-xl font-semibold mb-4">
+                        Personnes que vous suivez
+                    </h2>
+                    <ul>
+                        <li
+                            v-for="following in followings"
+                            :key="following.id"
+                            class="mb-2"
+                        >
+                            <Link
+                                :href="`/profile/${following.id}`"
+                                class="text-blue-600 hover:underline"
+                            >
+                                {{ following.name }}
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
     </AuthenticatedLayout>
 </template>
-
-<style>
-
-</style>
